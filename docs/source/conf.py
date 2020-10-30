6f0cfb72c8746663c6d54f45b7e8357636863443
datadavev <datadavev@users.noreply.github.com>
2020-10-30 12:39:59 -0400

    Switch to material theme, GH action build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,11 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'pallets_sphinx_themes',
   'sphinxcontrib.plantuml',
+  'sphinx_issues',
+  #'myst_nb',
+  'nbsphinx',
 ]
 
 # Configure plantuml
@@ -46,15 +50,36 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# GitHub repo
+issues_github_path = "isamplesorg/isamples-central"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'flask'
+html_theme = "sphinx_material"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    #"css/custom.css",
+]
+
+html_theme_options = {
+  'repo_url':'https://github.com/isamplesorg/isamples-central',
+  'repo_name':'iSamples-Central',
+  'globaltoc_depth': 3,
+  # If False, expand all TOC entries
+  'globaltoc_collapse': False,
+  # If True, show hidden TOC entries
+  'globaltoc_includehidden': True,
+}
+
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. iSamples-Central documentation master file, created by
+   sphinx-quickstart on Wed Sep 16 09:59:26 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+iSamples Central
+================
+
+iSamples Central (iSC) is designed as a permanent Internet service
+for sample metadata to help ensure reliable discovery and retrieval
+of physical sample metadata and related resources.
+
+**Notes:**
+
+.. toctree::
+   :numbered:
+   :maxdepth: 1
+
+   notes/igsn_counts
+   notes/igsn_metadata
